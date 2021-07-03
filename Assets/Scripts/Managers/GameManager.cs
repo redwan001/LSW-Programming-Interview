872a5dc2012941fdb7b86c7fd5e61484af559b1b
redwan001 <abir.cse.ku@gmail.com>
2021-07-03 17:14:18 +0600

    .exe build

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -30,7 +30,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
 
         if (gameMenuOpen || dailogeActive || shopActive)
         {
